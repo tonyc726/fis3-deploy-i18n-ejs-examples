33164d9743d8d2f8c8fdace3fed6c5374cb0948d
tony <zhujf620@gmail.com>
2017-08-17 18:14:39 +0800

    feat: 增加基础实验

---
+++ default/fis-conf.js
@@ -0,0 +1,54 @@
+const Path = require('path');
+
+// 初始化 F.I.S 配置项
+fis.set('project.ignore', [
+  'node_modules/**',
+  '.git/**',
+  '.svn/**',
+  'fis-conf.*',
+  'package.json',
+  'yarn.lock',
+  // project ignore files
+  'dist/**'
+]);
+
+// i18n静态模板编译的默认设置
+const I18N_EJS_OPTIONS = {
+  open: '<%',
+  close: '%>',
+  i18nDir: 'translations',
+  dist: '$lang/$file',
+  defaultLangName: '',
+  ignoreMatch: '',
+  notKeepOriginSubPathMatch: '/pages/*.html',
+  /**
+   * @desc 多语言文件处理函数
+   * @param {string} i18nFileJSONClone - 语言文件内容(JSON格式)的拷贝对象
+   * @param {string} fileLangName - 当前语言文件对应的语言名字
+   * @param {string} defaultLangName - 默认语言名字
+   * @return {object} 处理以后的文件内容
+   *
+   * @example
+   * onLangFileParse(i18nFileJSONClone, fileLangName, defaultLangName) {},
+   */
+  onLangFileParse: null,
+};
+
+// ------ 配置i18n
+fis.match('/translations/**', {
+  release: false
+});
+
+fis.match('/pages/(**)/(*.html)', {
+  release: '/$1/$2'
+});
+
+// deploy config
+fis.match('*', {
+  deploy: [
+    fis.plugin('i18n-ejs', I18N_EJS_OPTIONS),
+    fis.plugin('local-deliver', {
+      to: Path.resolve(__dirname, './dist')
+    })
+  ]
+});
